3ca1f314c03f77cb69e1bf2260bddc7dfc4a7146
DESKTOP-BSOJMUG\jinwoojung <jinu9561@gmail.com>
2024-06-19 14:44:01 +0900

    상단바에 알림 아이콘 추가

--- src/components/header/IconGroup.js
+++ src/components/header/IconGroup.js
@@ -22,6 +22,7 @@ const IconGroup = ({ iconWhiteClass }) => {
   const [showFriendList, setShowFriendList] = useState(false); // 친구
   const [receiverId, setReceiverId] = useState(''); // 친구 요청 ID 상태 추가
   const [message, setMessage] = useState(''); // 메시지 상태 추가
+  const [showNotifications, setShowNotifications] = useState(false); // 알림 창 상태 추가
 
   useEffect(() => {
     const storedUserId = localStorage.getItem('userId');
@@ -84,47 +85,47 @@ const IconGroup = ({ iconWhiteClass }) => {
     event.preventDefault();
     requestFriend();
   }
-  
-  const logoutCheck = (e)=>{ 
+
+  const logoutCheck = (e)=>{
       e.preventDefault();
 
-      let formData = new FormData(); //폼전송으로 보내기위한 작업 
-      formData.append("userId", saveData.userId); 
+      let formData = new FormData(); //폼전송으로 보내기위한 작업
+      formData.append("userId", saveData.userId);
 
     // 전송
-    axios({ 
-    method:"POST", 
+    axios({
+    method:"POST",
     url : "http://localhost:9000/logout",
     data : formData
-    }) 
-     .then((res)=>{ 
-        alert(res.data); 
+    })
+     .then((res)=>{
+        alert(res.data);
 
-        localStorage.removeItem("userId"); 
-        localStorage.removeItem("country"); 
-        localStorage.removeItem("gender"); 
+        localStorage.removeItem("userId");
+        localStorage.removeItem("country");
+        localStorage.removeItem("gender");
         localStorage.removeItem("userName");
-        localStorage.removeItem("userSeq"); 
-        localStorage.removeItem("nickName"); 
-        localStorage.removeItem("userJelly"); 
+        localStorage.removeItem("userSeq");
+        localStorage.removeItem("nickName");
+        localStorage.removeItem("userJelly");
         localStorage.removeItem("Authorization");
       // 공유된 변수를 상태를 변경하면 이 컨텍스트를 사용하는 모든 컴포넌트가 상태변경을 감지하고 업데이트 된다!!
-        logingedCon.onLoggedChange(false); 
+        logingedCon.onLoggedChange(false);
         navigator("/");
-  
-    }) 
-    .catch((err)=>{ console.log(err) 
-        let errMessage = err.response.data.type +"\n"; 
-        errMessage += err.response.data.title +"\n"; 
-        errMessage += err.response.data.detail +"\n"; 
-        errMessage += err.response.data.status +"\n"; 
-        errMessage += err.response.data.instance +"\n"; 
-        
-        errMessage += err.response.data.timestamp; 
-        alert(errMessage); 
+
+    })
+    .catch((err)=>{ console.log(err)
+        let errMessage = err.response.data.type +"\n";
+        errMessage += err.response.data.title +"\n";
+        errMessage += err.response.data.detail +"\n";
+        errMessage += err.response.data.status +"\n";
+        errMessage += err.response.data.instance +"\n";
+
+        errMessage += err.response.data.timestamp;
+        alert(errMessage);
     });
 
-       
+
  }
 
  const resign = () =>{
@@ -132,32 +133,32 @@ const IconGroup = ({ iconWhiteClass }) => {
   const check = window.confirm("정말 회원 탈퇴하시겠습니까?");
   // 전송
   if(check){
-    axios({ 
-      method:"GET", 
+    axios({
+      method:"GET",
       url : "http://localhost:9000/users/resign/"+saveData.userSeq,
-      }) 
-       .then((res)=>{ 
-          alert(res.data); 
-  
-          localStorage.removeItem("userId"); 
+      })
+       .then((res)=>{
+          alert(res.data);
+
+          localStorage.removeItem("userId");
           localStorage.removeItem("userName");
-          localStorage.removeItem("userSeq"); 
+          localStorage.removeItem("userSeq");
           localStorage.removeItem("Authorization");
           localStorage.removeItem("rememberedUserId");
           localStorage.removeItem("rememberedPassword");
         // 공유된 변수를 상태를 변경하면 이 컨텍스트를 사용하는 모든 컴포넌트가 상태변경을 감지하고 업데이트 된다!!
-        logingedCon.onLoggedChange(false); 
-  
+        logingedCon.onLoggedChange(false);
+
         navigator("/");
-       }) 
-       .catch((err)=>{ console.log(err) 
-            let errMessage = err.response.data.type +"\n"; 
-            errMessage += err.response.data.title +"\n"; 
-            errMessage += err.response.data.detail +"\n"; 
-            errMessage += err.response.data.status +"\n"; 
-            errMessage += err.response.data.instance +"\n"; 
-            errMessage += err.response.data.timestamp; 
-            alert(errMessage); 
+       })
+       .catch((err)=>{ console.log(err)
+            let errMessage = err.response.data.type +"\n";
+            errMessage += err.response.data.title +"\n";
+            errMessage += err.response.data.detail +"\n";
+            errMessage += err.response.data.status +"\n";
+            errMessage += err.response.data.instance +"\n";
+            errMessage += err.response.data.timestamp;
+            alert(errMessage);
       });
     }
   }
@@ -179,148 +180,164 @@ const IconGroup = ({ iconWhiteClass }) => {
   const { cartItems } = useSelector((state) => state.cart);
 
   return (
-    <div className={clsx("header-right-wrap", iconWhiteClass)} >
-      <div className="same-style header-search d-none d-lg-block">
-        <button className="search-active" onClick={e => handleClick(e)}>
-          <i className="pe-7s-search" />
-        </button>
-        <div className="search-content">
-          <form action="#">
-            <input type="text" placeholder="Search" />
-            <button className="button-search">
-              <i className="pe-7s-search" />
-            </button>
-          </form>
-        </div>
-      </div>
-      <div className="same-style account-setting d-none d-lg-block">
-        <button
-          className="account-setting-active"
-          onClick={e => handleClick(e)}
-        >
-          <i className="pe-7s-user-female" />
-        </button>
-        <div className="account-dropdown">
-          <ul>
-            <li>
-              { logingedCon.isLoggedIn ? <Link to="#" onClick={logoutCheck} className="nav-link">로그아웃</Link> :(<Link to={process.env.PUBLIC_URL + "/login-register"}>로그인</Link>)}
-            </li>
-            <li>
-              {!logingedCon.isLoggedIn && <Link to={process.env.PUBLIC_URL + "/login-register"}>
-                회원 가입
-              </Link>}
-            </li>
-            <li>
-              {!logingedCon.isLoggedIn && <Link to={process.env.PUBLIC_URL + "/emailVerification"}>
-                회원 인증
-              </Link>}
-            </li>
-            <li>
-              { logingedCon.isLoggedIn && <Link to={process.env.PUBLIC_URL + "/my-profile"}>
-                프로필
-              </Link>}
-            </li>
-            <li>
-              { logingedCon.isLoggedIn && <Link to={process.env.PUBLIC_URL + "/smsVerification"}>
-                본인 인증
-              </Link>}
-            </li>
-            <li>
-              { logingedCon.isLoggedIn && <Link to={process.env.PUBLIC_URL + "/jellyTransction"}>
-                젤리 구매
-              </Link>}
-            </li>
-            <li>
-              { logingedCon.isLoggedIn && <Link to="#" onClick={resign}>
-                회원 탈퇴
-              </Link>}
-            </li>
-            <li>
-              {logingedCon.isLoggedIn && <Link to="#" onClick={toggleFriendListHandler }>
-                친구 목록
-              </Link>}
-            </li>
+      <div className={clsx("header-right-wrap", iconWhiteClass)}>
 
-            {/* 친구요청 기능확인용 임시(채팅 완성되면 거기에 쓸거) */}
-            <li>
-              <input
-                type="text"
-                placeholder="친구 ID 입력"
-                value={receiverId}
-                onChange={(e) => setReceiverId(e.target.value)}
-              />
-              <button onClick={handleFriendRequestSubmit} style={{fontSize:"15px"}}>친구요청</button>
-            </li>
-            <li>
-              {message && <p>{message}</p>}
-            </li>
+          <div className="same-style header-notification d-none d-lg-block">
+              <button className="notification-active" onClick={e => handleClick(e)}>
+                  <i className="pe-7s-bell"/>
+              </button>
+              <div className="account-dropdown">
+                  <ul>
+                      <li>
+                          <p>새로운 알림이 없습니다.</p>
+                      </li>
+                  </ul>
+              </div>
+          </div>
 
-          </ul>
-        </div>
-      </div>
-      <div className="same-style header-compare">
-        <Link to={process.env.PUBLIC_URL + "/"}>
-          <i className="pe-7s-shuffle" />
-        </Link>
-      </div>
-      <div className="same-style header-wishlist">
-        <Link to={process.env.PUBLIC_URL + "/wishlist"}>
-          <i className="pe-7s-like" />
-          <span className="count-style">
+
+          <div className="same-style header-search d-none d-lg-block">
+              <button className="search-active" onClick={e => handleClick(e)}>
+                  <i className="pe-7s-search"/>
+              </button>
+              <div className="search-content">
+                  <form action="#">
+                      <input type="text" placeholder="Search"/>
+                      <button className="button-search">
+                          <i className="pe-7s-search"/>
+                      </button>
+                  </form>
+              </div>
+          </div>
+          <div className="same-style account-setting d-none d-lg-block">
+              <button
+                  className="account-setting-active"
+                  onClick={e => handleClick(e)}
+              >
+                  <i className="pe-7s-user-female"/>
+              </button>
+              <div className="account-dropdown">
+                  <ul>
+                      <li>
+                          {logingedCon.isLoggedIn ?
+                              <Link to="#" onClick={logoutCheck} className="nav-link">로그아웃</Link> : (
+                                  <Link to={process.env.PUBLIC_URL + "/login-register"}>로그인</Link>)}
+                      </li>
+                      <li>
+                          {!logingedCon.isLoggedIn && <Link to={process.env.PUBLIC_URL + "/login-register"}>
+                              회원 가입
+                          </Link>}
+                      </li>
+                      <li>
+                          {!logingedCon.isLoggedIn && <Link to={process.env.PUBLIC_URL + "/emailVerification"}>
+                              회원 인증
+                          </Link>}
+                      </li>
+                      <li>
+                          {logingedCon.isLoggedIn && <Link to={process.env.PUBLIC_URL + "/my-profile"}>
+                              프로필
+                          </Link>}
+                      </li>
+                      <li>
+                          {logingedCon.isLoggedIn && <Link to={process.env.PUBLIC_URL + "/smsVerification"}>
+                              본인 인증
+                          </Link>}
+                      </li>
+                      <li>
+                          {logingedCon.isLoggedIn && <Link to={process.env.PUBLIC_URL + "/jellyTransction"}>
+                              젤리 구매
+                          </Link>}
+                      </li>
+                      <li>
+                          {logingedCon.isLoggedIn && <Link to="#" onClick={resign}>
+                              회원 탈퇴
+                          </Link>}
+                      </li>
+                      <li>
+                          {logingedCon.isLoggedIn && <Link to="#" onClick={toggleFriendListHandler}>
+                              친구 목록
+                          </Link>}
+                      </li>
+
+                      {/* 친구요청 기능확인용 임시(채팅 완성되면 거기에 쓸거) */}
+                      <li>
+                          <input
+                              type="text"
+                              placeholder="친구 ID 입력"
+                              value={receiverId}
+                              onChange={(e) => setReceiverId(e.target.value)}
+                          />
+                          <button onClick={handleFriendRequestSubmit} style={{fontSize: "15px"}}>친구요청</button>
+                      </li>
+                      <li>
+                          {message && <p>{message}</p>}
+                      </li>
+
+                  </ul>
+              </div>
+          </div>
+          <div className="same-style header-compare">
+              <Link to={process.env.PUBLIC_URL + "/"}>
+                  <i className="pe-7s-shuffle"/>
+              </Link>
+          </div>
+          <div className="same-style header-wishlist">
+              <Link to={process.env.PUBLIC_URL + "/wishlist"}>
+                  <i className="pe-7s-like"/>
+                  <span className="count-style">
             {wishlistItems && wishlistItems.length ? wishlistItems.length : 0}
           </span>
-        </Link>
-      </div>
-      <div className="same-style cart-wrap d-none d-lg-block">
-        <button className="icon-cart" onClick={e => handleClick(e)}>
-          <i className="pe-7s-shopbag" />
-          <span className="count-style">
+              </Link>
+          </div>
+          <div className="same-style cart-wrap d-none d-lg-block">
+              <button className="icon-cart" onClick={e => handleClick(e)}>
+                  <i className="pe-7s-shopbag"/>
+                  <span className="count-style">
             {cartItems && cartItems.length ? cartItems.length : 0}
           </span>
-        </button>
-        {/* menu cart */}
-        <MenuCart />
-      </div>
-      <div className="same-style cart-wrap d-block d-lg-none">
-        <Link className="icon-cart" to={process.env.PUBLIC_URL + "/cart"}>
-          <i className="pe-7s-shopbag" />
-          <span className="count-style">
+              </button>
+              {/* menu cart */}
+              <MenuCart/>
+          </div>
+          <div className="same-style cart-wrap d-block d-lg-none">
+              <Link className="icon-cart" to={process.env.PUBLIC_URL + "/cart"}>
+                  <i className="pe-7s-shopbag"/>
+                  <span className="count-style">
             {cartItems && cartItems.length ? cartItems.length : 0}
           </span>
-        </Link>
-      </div>
-      <div className="same-style mobile-off-canvas d-block d-lg-none">
-        <button
-          className="mobile-aside-button"
-          onClick={() => triggerMobileMenu()}
-        >
-          <i className="pe-7s-menu" />
-        </button>
-      </div>
+              </Link>
+          </div>
+          <div className="same-style mobile-off-canvas d-block d-lg-none">
+              <button
+                  className="mobile-aside-button"
+                  onClick={() => triggerMobileMenu()}
+              >
+                  <i className="pe-7s-menu"/>
+              </button>
+          </div>
 
-      {showFriendList && ( // 친구목록
-        <div className="offcanvas offcanvas-end show" tabIndex="-1" style={{ visibility: "visible" }}>
-          <div className="offcanvas-header">
-            <h5 className="offcanvas-title">친구 목록</h5>
-            <button type="button" className="btn-close text-reset" onClick={toggleFriendListHandler}></button>
-          </div>
-          <div className="offcanvas-body">
-            <ul>
-              {friendList.map(friend => (
-                <li key={friend.userSeq}>{friend.userName}</li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      )}
-    </div>
+          {showFriendList && ( // 친구목록
+              <div className="offcanvas offcanvas-end show" tabIndex="-1" style={{visibility: "visible"}}>
+                  <div className="offcanvas-header">
+                      <h5 className="offcanvas-title">친구 목록</h5>
+                      <button type="button" className="btn-close text-reset" onClick={toggleFriendListHandler}></button>
+                  </div>
+                  <div className="offcanvas-body">
+                      <ul>
+                          {friendList.map(friend => (
+                              <li key={friend.userSeq}>{friend.userName}</li>
+                          ))}
+                      </ul>
+                  </div>
+              </div>
+          )}
+      </div>
   );
 };
 
 IconGroup.propTypes = {
-  iconWhiteClass: PropTypes.string,
+    iconWhiteClass: PropTypes.string,
 };
-
 
 
 export default IconGroup;
